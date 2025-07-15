aa86fb4311c7a0510ea6ae31edbdf4c548859e0c
0sipov <smozoluk@gmail.com>
2025-07-15 20:26:51 +0300

    commit

--- index.js
+++ index.js
@@ -3,18 +3,25 @@ const axios = require('axios');
 
 const token = process.env.MONOBANK_TOKEN;
 
-async function getClientInfo() {
+const takeClientInfo = (response) => {console.log(response.data); return};
+const takeBalanceInfo = (response, bankAccountPath) => {console.log(response.data.jars.filter(jar => jar.title === bankAccountPath)); return};
+
+const BuisnesPath = "На бізнес"
+
+async function getClientInfo(bankAccountPath) {
   try {
     const response = await axios.get('https://api.monobank.ua/personal/client-info', {
       headers: {
         'X-Token': token
       }
     });
-    const balanceInfoForBuisnes = response.data.jars.filter(jar => jar.title === 'На бізнес')
-    console.log(balanceInfoForBuisnes);
+
+    if (bankAccountPath) {takeBalanceInfo(response, bankAccountPath)}
+    else {takeClientInfo(response)}
+
   } catch (error) {
     console.error('Error:', error.response?.data || error.message);
   }
 }
 
-getClientInfo()
+getClientInfo(BuisnesPath)
